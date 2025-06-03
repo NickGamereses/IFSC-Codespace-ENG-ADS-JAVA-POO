326d3f425148ecd3b5713ce397dfcd79e36748fc
NickGamereses <136147518+NickGamereses@users.noreply.github.com>
2025-06-03 01:08:14 +0000

    aula 2/6/25

---
+++ A0_HelloWorld.java
@@ -0,0 +1,8 @@
+public class A0_HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");     {
+        System.out.println("Hahahaha");
+    
+}
+    }
+}
--- A1_0Teste.java
+++ A1_0Teste.java
@@ -1,10 +1,13 @@
 public class A1_0Teste {
     public static void main(String[] args) {
         // Declaração de variáveis inteiras e reais
-        int num1 = 10;      // Tipo inteiro (int)
-        double num2 = 10.5;      // Tipo real 
+        int num1 = 19;      // Tipo inteiro (int)
+        double num2 = 1;
+        double soma = (num1 + num2);    // Tipo real 
         // Exibindo os valores das variáveis
-        System.out.println("Esse é o num1 " + num1);
-        System.out.println("Esse é o num2 " + num2);
+        System.out.println("A soma é " + soma);
+        if (soma > 15) {
+        System.out.println("A soma é maior que 15 ");
+     } else {System.out.println("A soma não é maior que 15");}
     }
 }
